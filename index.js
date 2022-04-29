8f287789fe3f33e73e030d413eab6b7192880d0f
Developer-Barsha <lamiatabassumbarsha@gmail.com>
2022-04-29 22:08:35 +0600

    get fruit by id

--- index.js
+++ index.js
@@ -25,6 +25,13 @@ async function run(){
             res.send(fruits);
         })
 
+        app.get('/fruits/:id', async(req, res)=>{
+            const id = req.params.id;
+            const query = {_id : ObjectId(id)};
+            const fruit = await fruitsCollection.findOne(query);
+            res.send(fruit);
+        })
+
         console.log('connected to db');
     }
     finally{
