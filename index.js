ac4900d315b36b55d3626c97a375eff72fd1d468
Developer-Barsha <lamiatabassumbarsha@gmail.com>
2022-04-30 22:24:32 +0600

    update api done

--- index.js
+++ index.js
@@ -13,44 +13,60 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.ldw54.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-async function run(){
+async function run() {
     await client.connect();
     const fruitsCollection = client.db("fruitsCorner").collection("fruit");
 
-    try{
-        app.get('/fruits', async(req, res)=>{
+    try {
+        // get all fruits
+        app.get('/fruits', async (req, res) => {
             const email = req.query.email;
-            const query = {email:email};
+            const query = { email: email };
             const cursor = fruitsCollection.find(query);
             const fruits = await cursor.toArray();
             res.send(fruits);
         })
 
-        app.post('/fruits', async(req, res)=>{
+        // post fruit api
+        app.post('/fruits', async (req, res) => {
             const query = req.body;
             const fruits = await fruitsCollection.insertOne(query);
-            res.send(fruits); 
+            res.send(fruits);
         })
 
-        app.get('/fruits/:id', async(req, res)=>{
+        // get fruit by id
+        app.get('/fruits/:id', async (req, res) => {
             const id = req.params.id;
-            const query = {_id : ObjectId(id)};
+            const query = { _id: ObjectId(id) };
             const fruit = await fruitsCollection.findOne(query);
             res.send(fruit);
         })
 
+        // update fruit info
+        app.put('/fruits/:id', async (req, res) => {
+            const id = req.params.id;
+            const updatedFruit = req.body;
+            const query = { _id: ObjectId(id) };
+            const options = { upsert: true };
+            const updateDoc = {
+                $set: {updatedFruit},
+            };
+            const result = await fruitsCollection.updateOne(query, updateDoc, options);
+            res.send(result);
+        })
+
         console.log('connected to db');
     }
-    finally{
+    finally {
 
     }
 }
 run().catch(console.dir);
 
-app.get('/', (req, res)=>{
+app.get('/', (req, res) => {
     res.send('Running Fruits Corner server')
 })
 
-app.listen(port, ()=>{
+app.listen(port, () => {
     console.log('listening to port:- ', port);
 })
